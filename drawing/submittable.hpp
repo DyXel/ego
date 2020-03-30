87508ab2d50a50bfbaf766c351bee6b9f3a67fc2
Dylam De La Torre <dyxel04@gmail.com>
2020-03-30 05:42:48 +0200

    add missed submittable.hpp

---
+++ drawing/submittable.hpp
@@ -0,0 +1,43 @@
+#ifndef DRAWING_SUBMITTABLE
+#define DRAWING_SUBMITTABLE
+#include <array>
+#include <cstddef> // std::size_t
+#include <vector>
+
+namespace Drawing
+{
+
+namespace Detail
+{
+
+template<typename T>
+class Submittable
+{
+public:
+	using ValueType = T;
+	
+	virtual void Submit(const T* data, std::size_t length) = 0;
+	
+	template<std::size_t Length>
+	inline void Submit(const T(&data)[Length])
+	{
+		Submit(data, Length);
+	}
+	
+	template<std::size_t Length>
+	inline void Submit(const std::array<T, Length>& array)
+	{
+		Submit(array.data(), Length);
+	}
+	
+	inline void Submit(const std::vector<T>& vec)
+	{
+		Submit(vec.data(), vec.size());
+	}
+};
+
+} // namespace Detail
+
+} // namespace Drawing
+
+#endif // DRAWING_SUBMITTABLE
